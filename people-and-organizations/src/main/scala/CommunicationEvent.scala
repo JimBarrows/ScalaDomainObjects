315c8a67acd667b63986bc276693ce09cdcccaa8
Jim Barrows <Jim.Barrows@gmail.com[color]>
2013-09-12 17:57:55 -0700

    Figure 2.12 complete

---
+++ people-and-organizations/src/main/scala/CommunicationEvent.scala
@@ -0,0 +1,36 @@
+package sdo.peopleAndOrganizations.communicationEvent
+
+import sdo.peopleAndOrganizations.{Party, Relationship}
+import sdo.peopleAndOrganizations.contactMechanisms.ContactMechanism
+import sdo.core.domain._
+
+class CommunicationEvent( initialId: EntityUuidIdField, 
+													happensVia: ContactMechanism,
+													inContextOf: Relationship) extends Entity{
+
+	override val id = initialId
+
+	val started = DateTimeField
+	val ended = DateTimeField
+	val note = TextField
+	val purpose = PurposeField
+	val involving =  new ListField[Role] ()
+	def via = happensVia
+	def context = inContextOf 
+}
+
+class Role( description: String, party: Party)
+
+class RoleField extends Field[ Role]
+
+object RoleField {
+	def apply = new RoleField()
+}
+
+class Purpose( description: String)
+
+class PurposeField extends Field[ Purpose]
+
+object PurposeField {
+	def apply = new PurposeField()
+}
