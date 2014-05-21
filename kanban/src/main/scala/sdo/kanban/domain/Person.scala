be3f6235f3045f0bc93ae6f65c19b756efbd4f0d
Jim Barrows <Jim.Barrows@gmail.com>
2014-05-21 06:57:57 -0700

    Committing to change os's

--- kanban/src/main/scala/sdo/kanban/domain/Person.scala
+++ kanban/src/main/scala/sdo/kanban/domain/Person.scala
@@ -6,11 +6,14 @@ import sdo.peopleAnOrganizations.domain.Role
 import sdo.peopleAnOrganizations.domain.{Person => PartyPerson}
 import sdo.workEffort.domain.Party
 import sdo.peopleAnOrganizations.domain.PartyRole
+import sdo.workEffort.domain.Worker
 
 class Person(initialId: EntityUuidIdField) extends PartyPerson(initialId) with Party {
 
   override val id = this.initialId
 
-   override def actingAs = this.roles
+   override def actingAs:List[Worker] = this.roles.list.filter( r => 
+       r.isInstanceOf[Worker])
+       ).getOrElse(Nil).asInstanceOf[List[Worker]]
 
 }
