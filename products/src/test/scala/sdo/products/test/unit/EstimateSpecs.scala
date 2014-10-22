429133586021e77b706bcce3e94e7866dbdf73f1
Jim Barrows <Jim.Barrows@gmail.com>
2014-10-22 05:39:16 -0700

    More product related specs.

---
+++ products/src/test/scala/sdo/products/test/unit/EstimateSpecs.scala
@@ -0,0 +1,43 @@
+package sdo.products.test.unit
+
+import org.specs2.mutable.Specification
+import scalaz.Scalaz._
+import sdo.core.domain._
+import sdo.peopleAnOrganizations.domain._
+
+class EstimateSpecs extends Specification{
+
+	"An estimate" should {
+		"have a date range it's good for" in {
+			pending
+		}
+		
+		"have a cost" in {
+			pending
+		}
+		
+		"be calculated for a product xor product feature" in {
+			pending
+		}
+		
+		"optionally be specified for a geographic boundary" in {
+			pending
+		}
+		
+		"specified for an organization"  in {
+			pending
+		}
+		
+		"have Estimated Materials cost as a sub-type"  in {
+			pending
+		}
+		
+		"have Estimated Labor cost as a sub-type" in {
+			pending
+		}
+		
+		"have Estimated Other Costs as a sub-type"  in {
+			pending
+		}
+	}
+}
---
+++ products/src/test/scala/sdo/products/test/unit/ProductToProductAssociationSpecs.scala
@@ -0,0 +1,85 @@
+package sdo.products.test.unit
+
+import org.specs2.mutable.Specification
+import scalaz.Scalaz._
+import sdo.core.domain._
+import sdo.peopleAnOrganizations.domain._
+
+class ProductToProductAssociationSpecs  extends Specification{
+
+	"A product to product association" should {
+		"optionally be made up of product components" in {
+			pending
+		}
+		
+		"optionally be made up of product substitute" in {
+			pending
+		}
+		
+		"optionally be made up of product obsolences" in {
+			pending
+		}
+		
+		"optionally be made up of product complements" in {
+			pending
+		}
+		
+		"optionally be made up of product incompatbility" in {
+			pending
+		}
+	}
+	
+	"A product component" should {
+		"have a date range it's good for" in {
+			pending
+		}
+		"have a quantity used" in {
+			pending
+		}
+		"have instructions" in {
+			pending
+		}
+		"optionally have comments" in {
+			pending
+		}
+	}
+	
+	"A product substitute" should {
+		"have a date range it's good for" in {
+			pending
+		}
+		"have a quantity" in {
+			pending
+		}
+		"optionally have comments" in {
+			pending
+		}
+	}
+	
+	"A product obsolesence" should {
+		"Have a supression date" in {
+			pending
+		}
+		"have a reason" in {
+			pending
+		}
+	}
+	
+	"A product complement" should {
+		"have a date range" in {
+			pending
+		}
+		"have a reason" in {
+			pending
+		}
+	}
+	
+	"A product incompatibility" should {
+		"have a date range" in {
+			pending
+		}
+		"have a reason" in {
+			pending
+		}
+	}
+}
